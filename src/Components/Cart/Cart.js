20e4b3731dff734893f9913974ccf9e688277168
Arpan Sen <warpan2020@gmail.com>
2024-04-22 20:43:01 +0530

    added a cart which shows items with pagination

--- src/Components/Cart/Cart.js
+++ src/Components/Cart/Cart.js
@@ -1,15 +1,37 @@
-import {Badge,Drawer} from 'antd';
+import {Badge,Drawer, Table} from 'antd';
 import {ShoppingCartOutlined} from '@ant-design/icons';
-import {useState} from 'react';
+import {useState, useEffect} from 'react';
+
 
 function Cart(){
 
     const [cartOpenStatus,setCartOpenStatus] = useState(false);
+    const [cartItems, setCartItems] = useState({});
+
+    const fetchCartItems = async () => {
+        const response = await fetch('https://fakestoreapi.com/carts/user/2');
+        const data = await response.json();
+        setCartItems(data);
+    };
+    useEffect(() => {fetchCartItems()} , []);
+    console.log(cartItems);
     return (
         <div>
-        <Badge count={3} style={{marginRight:'10px'}}>
+        <Badge count={cartItems[0].products.length} style={{marginRight:'10px', cursor:'pointer'}}>
             <ShoppingCartOutlined onClick={() => setCartOpenStatus(true)} style={{fontSize:'30px', marginRight:'10px'}}/>
-            <Drawer open={cartOpenStatus} onClose={() => setCartOpenStatus(false)}></Drawer>
+            <Drawer title="My Cart"open={cartOpenStatus} onClose={() => setCartOpenStatus(false)}>
+          <Table  columns={[
+               {
+        title:"Product Id",
+        dataIndex: "productId"
+              },
+              {
+        title:"Quantity"
+        , dataIndex: "quantity"
+               }
+           ]}  dataSource={cartItems[0].products}></Table>
+
+            </Drawer>
             </Badge>
             </div>
     );
