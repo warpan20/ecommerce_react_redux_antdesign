626c4f6d40edd45b755a338b332fa5b80db4e2b2
Arpan Sen <warpan2020@gmail.com>
2024-04-24 16:18:13 +0530

    label coverted to icon in header menu items

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -14,13 +14,13 @@ function Header() {
       
     return (<div class="menu" style={{height:'48px',position:'fixed',zIndex: 1,top:'5px',width:'98%', right:'1%', boxShadow: '0 5px 10px 0 rgba(0, 0, 0, 0.5)', display:'flex' , flexDirection: 'row'
     , alignItems:'center', backgroundColor:'white',justifyContent:'space-between'}} > 
-           <Menu mode="horizontal" onClick={onMenuClick} items={
+           <Menu mode="horizontal" inlineCollapsed={true} onClick={onMenuClick} items={
             [
-                {label: <HomeFilled style={{fontSize: '20px'}}/>,key: "home"},
-                {label:  <MobileOutlined style={{fontSize: '20px'}}/>,key:"electronics"},
-                {label: <ManOutlined style={{fontSize: '20px'}}/>,key:"mensclothing"},
-                {label:  <WomanOutlined style={{fontSize: '20px'}}/>,key:"womensclothing"},
-                {label:  <CrownOutlined style={{fontSize: '20px'}}/>,key:"jewelery"}
+                {icon: <HomeFilled style={{fontSize: '20px'}}/>,key: "home"},
+                {icon:  <MobileOutlined style={{fontSize: '20px'}}/>,key:"electronics"},
+                {icon: <ManOutlined style={{fontSize: '20px'}}/>,key:"mensclothing"},
+                {icon:  <WomanOutlined style={{fontSize: '20px'}}/>,key:"womensclothing"},
+                {icon:  <CrownOutlined style={{fontSize: '20px'}}/>,key:"jewelery"}
             ]
            }>
         </Menu>
